f3b370104e61a9f7aa14a81c7c73a8a568f7fe24
elenana7arova <elena.na7arova@gmail.com>
2021-01-15 14:10:26 +0300

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Elena Nazarova on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
